7079f1be8c4f7f6b52f1c0c620a92274a077bda9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-09 10:40:25 +0200

    fix: pass GITHUB_TOKEN to build auth service

--- shlokas.build.sh
+++ shlokas.build.sh
@@ -6,7 +6,7 @@ fi
 
 (cd ../shlokas-db && docker compose build) && \
 (cd ../shlokas-db && docker compose -f ./docker-compose.aux.yml build) && \
-(cd ../shlokas-auth && docker compose build) && \
+(cd ../shlokas-auth && docker compose build --build-arg GITHUB_TOKEN=${GITHUB_TOKEN}) && \
 (cd ../shlokas-landing && docker compose build) && \
 (cd ../shlokas-content && docker compose build) && \
 (cd ../shlokas-admin && docker compose build --build-arg GITHUB_TOKEN=${GITHUB_TOKEN}) && \
